691aab3876113a33a0f91e7954f87032d63930f3
Victor Javier Vasquez Trauco <VVASQUEZT2@UPAO.EDU.PE>
2023-04-23 02:19:28 -0500

    feat: Crear clase Food con los atributos name, description y price.

---
+++ api-food-delivery/src/main/java/com/example/apifooddelivery/ApiFoodDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.apifooddelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFoodDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFoodDeliveryApplication.class, args);
+	}
+
+}
---
+++ api-food-delivery/src/main/java/com/example/apifooddelivery/domain/Food.java
@@ -0,0 +1,24 @@
+package com.example.apifooddelivery.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "foods")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "description", nullable = false)
+    private String description;
+    @Column(name = "price", nullable = false)
+    private double price;
+}
